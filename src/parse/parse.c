c16c51a53d86fef7c2b59a82f7218b99c6a3eadd
asando <alexando17@yahoo.com>
2026-01-25 16:58:06 +0100

    working on adding and creating new_cmd and new_redir on the struct

--- src/parse/parse.c
+++ src/parse/parse.c
@@ -6,7 +6,7 @@
 /*   By: asando <asando@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/25 13:15:15 by asando            #+#    #+#             */
-/*   Updated: 2026/01/25 15:30:11 by asando           ###   ########.fr       */
+/*   Updated: 2026/01/25 16:57:27 by asando           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,5 +40,49 @@ t_redir	*ft_new_redir(t_token_type type, char *filename)
 	return (new_redir);
 }
 
-void	ft_add_cmd(){}
-void	ft_add_redir(){}
+//NOTE: Error when failed need to be considered
+void	ft_add_arg(t_cmd *cmd, char *value)
+{
+	int		count;
+	int		i;
+	char	**new_argv;
+
+	count = 0;
+	i = 0;
+	if (cmd->argv)
+	{
+		while (cmd->argv[count])
+			count++;
+	}
+	new_argv = malloc(sizeof(char *) * (count + 2));
+	if (new_argv == NULL)
+		return ;
+	while (cmd->argv[i])
+	{
+		new_argv[i] = cmd->argv[i];
+		i++;
+	}
+	new_argv[i] = value;
+	new_argv[i + 1] = NULL;
+	//BUG: Free the cmd->argv
+	cmd->argv = new_argv;
+	return ;
+}
+
+void	ft_add_redir(t_cmd *cmd, t_token_type type, char *filename)
+{
+	t_redir	*new_redir;
+	t_redir	*tmp;
+
+	new_redir = ft_new_redir(type, filename);
+	if (cmd->redirs == NULL)
+	{
+		cmd->redirs = new_redir;
+		return ;
+	}
+	tmp = cmd->redirs;
+	while (tmp->next_redir)
+		tmp = tmp->next_redir;
+	tmp = new_redir;
+	return ;
+}
