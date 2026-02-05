df8169776f5b39051b808394e767e19547528a88
asando <alexando17@yahoo.com>
2026-02-05 19:18:19 +0100

    Creating dedicated header for eecution

---
+++ include/execution.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   execution.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: asando <asando@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/05 19:11:21 by asando            #+#    #+#             */
+/*   Updated: 2026/02/05 19:16:40 by asando           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef EXECUTION_H
+# define EXECUTION_H
+# include "lexer.h"
+# include "parse.h"
+# include "libft.h"
+# include <unistd.h>
+# include <fcntl.h>
+# include <stdlib.h>
+
+#endif
