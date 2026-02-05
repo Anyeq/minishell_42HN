f5cbf8ed7bb5b760d125c5998cd30c94c00a0d36
asando <alexando17@yahoo.com>
2026-02-05 19:09:57 +0100

    Creating redirection function

--- src/execution/execution.c
+++ src/execution/execution.c
@@ -6,7 +6,7 @@
 /*   By: asando <asando@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/05 13:55:38 by asando            #+#    #+#             */
-/*   Updated: 2026/02/05 14:48:52 by asando           ###   ########.fr       */
+/*   Updated: 2026/02/05 19:09:23 by asando           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -58,4 +58,76 @@ int	ft_single_heredoc(t_redir *redirs)
 	close(pipe_fd[1]);
 	redirs->fd = pipe_fd[0];
 	return (0);
+}
+
+int	ft_cmd_heredoc(t_cmd *cmd)
+{
+	t_redir	*redir;
+
+	redir = cmd->redirs;
+
+	while (redir)
+	{
+		if (redir->type == TOKEN_HEREDOC)
+		{
+			if (ft_single_heredoc(redir) == -1)
+				return (-1);
+		}
+		redir = redir->next_redir;
+	}
+	return (0);
+}
+
+int	ft_prepare_heredoc(t_cmd *pipeline)
+{
+	t_cmd	*curr;
+
+	curr = pipeline;
+	while (curr)
+	{
+		if (ft_cmd_heredoc(curr) == -1)
+			return (-1);
+		curr = curr->next_cmd;
+	}
+	return (0);
+}
+
+void	ft_redirection_function(t_redir *redirs)
+{
+	int		fd;
+
+	while (redirs)
+	{
+		if (redirs->type == TOKEN_REDIR_IN)
+		{
+			fd = open(redirs->filename, O_RDONLY);
+			if (fd == -1)
+				exit(1);
+			dup2(fd, STDIN_FILENO);
+			close(fd);
+		}
+		else if (redirs->type == TOKEN_REDIR_OUT)
+		{
+			fd = open(redirs->filename, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+			if (fd == -1)
+				exit(1);
+			dup2(fd, STDOUT_FILENO);
+			close(fd);
+		}
+		else if (redirs->type == TOKEN_APPEND)
+		{
+			fd = open(redirs->filename, O_WRONLY | O_CREAT | O_APPEND, 0644);
+			if (fd == -1)
+				exit(1);
+			dup2(fd, STDOUT_FILENO);
+			close(fd);
+		}
+		else if (redirs->type == TOKEN_HEREDOC)
+		{
+			dup2(redirs->fd, STDIN_FILENO);
+			close(redirs->fd);
+		}
+		redirs = redirs->next_redir;
+	}
+	return ;
 }
