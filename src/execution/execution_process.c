7571a6344cda74bd780e324112ade6939bf66904
asando <alexando17@yahoo.com>
2026-02-06 18:57:32 +0100

    create pipe allocation (dynamically)

--- src/execution/execution_process.c
+++ src/execution/execution_process.c
@@ -6,7 +6,7 @@
 /*   By: asando <asando@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/06 17:25:06 by asando            #+#    #+#             */
-/*   Updated: 2026/02/06 18:31:17 by asando           ###   ########.fr       */
+/*   Updated: 2026/02/06 18:56:54 by asando           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,27 +25,88 @@ int	ft_cmd_count(t_cmd *cmd)
 	return (res);
 }
 
-//NOTE: when pipe error should close the rest
-void	ft_create_pipe(int *pipes, int n_cmd)
+void	ft_clean_allocation(int n, int **address)
 {
 	int	i;
 
 	i = 0;
-	while (i < n_cmd)
+	while (i < n)
 	{
-		if (pipe(pipes[i]) == -1)
-		{
-			perror("pipe error");
-			exit(1);
-		}
+		free(address[i]);
 		i++;
 	}
 	return ;
 }
 
+void	ft_close_pipes(int n, int **pipes)
+{
+	int	i;
+
+	i = 0;
+	while (i < n)
+	{
+		close(pipes[i]);
+		i++;
+	}
+	return ;
+}
+
+int	**ft_allocate_pipes(int n_cmd)
+{
+	int	**pipes;
+	int	i;
+
+	i = 0;
+	pipes = malloc(sizeof(int *) * (n_cmd - 1));
+	if (pipes == NULL)
+	{
+		perror("malloc error");
+		return (NULL);
+	}
+	while (i < n_cmd - 1)
+	{
+		pipes[i] = malloc(sizeof(int) * 2);
+		if (pipes[i] == NULL)
+		{
+			perror("malloc error");
+			ft_clean_allocation(i, pipes);
+			free(pipes);
+			return (NULL);
+		}
+		i++;
+	}
+	return (pipes);
+}
+
+//NOTE: when pipe error should close the rest
+//NOTE: when NULL should clean everything
+int	**ft_create_pipe(int n_cmd)
+{
+	int	i;
+	int	**pipes;
+
+	i = 0;
+	pipes = ft_allocate_pipes(n_cmd);
+	if (pipes == NULL)
+		return (NULL);
+	while (i < n_cmd)
+	{
+		if (pipe(pipes[i]) == -1)
+		{
+			perror("pipe error");
+			ft_close_pipes(i, pipes);
+			ft_clean_allocation(i, pipes);
+			free(pipes);
+			return (NULL);
+		}
+		i++;
+	}
+	return (pipes);
+}
+
 //NOTE: Protect all dup
 //NOTE: if ft_redirection can be failed
-void	ft_child_process(t_cmd *cmd, int i, int n_cmd, int *pipes)
+void	ft_child_process(t_cmd *cmd, int i, int n_cmd, int **pipes)
 {
 	int	n;
 
@@ -68,7 +129,7 @@ void	ft_child_process(t_cmd *cmd, int i, int n_cmd, int *pipes)
 }
 
 //NOTE: check if after exit memory is cleaned
-void	ft_run_child_process(t_cmd *cmd, int n_cmd, int *pids, int *pipes)
+void	ft_run_child_process(t_cmd *cmd, int n_cmd, int *pids, int **pipes)
 {
 	int		i;
 
