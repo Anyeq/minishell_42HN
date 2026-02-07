18a83e443e178609ebd1d1848df2231da6d45b39
asando <alexando17@yahoo.com>
2026-02-07 19:50:20 +0100

    ADD: adding todo list on main.c

--- src/execution/execution_executor.c
+++ src/execution/execution_executor.c
@@ -6,7 +6,7 @@
 /*   By: asando <asando@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/07 19:10:55 by asando            #+#    #+#             */
-/*   Updated: 2026/02/07 19:11:23 by asando           ###   ########.fr       */
+/*   Updated: 2026/02/07 19:46:03 by asando           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,11 +22,15 @@ void	ft_executor(t_cmd *pipeline)
 
 	n_cmd = ft_cmd_count(pipeline);
 	pids = malloc(sizeof(pid_t) * n_cmd);
+	pipes = NULL;
 	if (pids == NULL)
 		return ;
-	pipes = ft_create_pipe(n_cmd);
-	if (pipes == NULL)
-		return ;
+	if (n_cmd > 1)
+	{
+		pipes = ft_create_pipe(n_cmd);
+		if (pipes == NULL)
+			return ;
+	}
 	ft_run_child_process(pipeline, n_cmd, pids, pipes);
 	ft_parent_process(pipes, pids, n_cmd);
 	ft_clean_pipe_allocation(n_cmd - 1, pipes);
--- src/main.c
+++ src/main.c
@@ -6,8 +6,19 @@
 /*   By: asando <asando@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/18 19:26:20 by asando            #+#    #+#             */
-/*   Updated: 2026/01/18 19:26:44 by asando           ###   ########.fr       */
+/*   Updated: 2026/02/07 19:49:39 by asando           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+int	main(void)
+{
+	// TODO: read
+	// TODO: Creat token
+	// TODO: create pipeline
+	// TODO: remove token
+	// TODO: executor --> pipeline cleaned here
+	// TODO: Repeat
+	return ;
+}
