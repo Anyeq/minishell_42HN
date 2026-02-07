c34bc5cadf6baac0ffd26aa1c5c786191df05893
asando <alexando17@yahoo.com>
2026-02-07 19:33:25 +0100

    FIX: Fixing typo und norminette failed

--- src/execution/execution_parent_process.c
+++ src/execution/execution_parent_process.c
@@ -6,7 +6,7 @@
 /*   By: asando <asando@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/07 11:35:13 by asando            #+#    #+#             */
-/*   Updated: 2026/02/07 19:11:25 by asando           ###   ########.fr       */
+/*   Updated: 2026/02/07 19:32:50 by asando           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,10 +39,10 @@ static int	ft_get_exit_status(int exit_status)
 	return (1);
 }
 
-int	ft_parent_process(int **pipes, pid_t *pids, int	n_cmd)
+int	ft_parent_process(int **pipes, pid_t *pids, int n_cmd)
 {
 	int	status;
-	int exit_code;
+	int	exit_code;
 
 	ft_close_pipes(n_cmd - 1, pipes);
 	status = ft_wait_children_process(pids, n_cmd);
--- src/execution/execution_process_utils.c
+++ src/execution/execution_process_utils.c
@@ -6,7 +6,7 @@
 /*   By: asando <asando@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/06 17:25:06 by asando            #+#    #+#             */
-/*   Updated: 2026/02/07 19:16:41 by asando           ###   ########.fr       */
+/*   Updated: 2026/02/07 19:20:58 by asando           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,6 +30,8 @@ void	ft_clean_pipe_allocation(int n, int **address)
 	int	i;
 
 	i = 0;
+	if (address == NULL)
+		return ;
 	while (i < n)
 	{
 		free(address[i]);
