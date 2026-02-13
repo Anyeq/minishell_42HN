19c281d1469ad75fe6b854155e4ab3f22ff659f1
asando <alexando17@yahoo.com>
2026-02-13 16:31:40 +0100

    ADD: add ft_get_env_value to get the value von VAR value

--- src/expansion/expansion.c
+++ src/expansion/expansion.c
@@ -6,7 +6,7 @@
 /*   By: asando <asando@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/13 12:10:52 by asando            #+#    #+#             */
-/*   Updated: 2026/02/13 15:58:10 by asando           ###   ########.fr       */
+/*   Updated: 2026/02/13 16:28:37 by asando           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,6 +29,22 @@ char	*ft_join_and_free(char *str1, char *str2)
 	return (res);
 }
 
+char	*ft_get_env_value(char **envp, char *key)
+{
+	int	len;
+	int	i;
+
+	i = 0;
+	len = ft_strlen(key);
+	while (envp[i])
+	{
+		if (ft_strncmp(envp[i], key, len) == 0 && envp[i][len] == '=')
+			return (envp[i] + len + 1);
+		i++;
+	}
+	return (NULL);
+}
+
 char	*ft_expand_value(char *value, char **envp, int exit_status)
 {
 	char	*result;
@@ -36,7 +52,10 @@ char	*ft_expand_value(char *value, char **envp, int exit_status)
 
 	result = ft_strdup("");
 	if (result == NULL)
+	{
+		perror("malloc error");
 		return (NULL);
+	}
 	i = 0;
 	while (value[i])
 	{
@@ -46,8 +65,16 @@ char	*ft_expand_value(char *value, char **envp, int exit_status)
 			if (value[i] == '?')
 			{
 				result = ft_join_and_free(result, ft_itoa(exit_status));
+				if (result == NULL)
+					return (NULL);
 				i++;
 			}
+			else if (ft_isalpha(value[i] || value[i] == '_'))
+			{
+				
+			}
+			else
+				result = ft_join_and_free(result, ft_strdup("$"));
 		}
 	}
 }
