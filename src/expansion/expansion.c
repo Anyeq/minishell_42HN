39615651b936f17bfefb838be0eb75f28a92b57f
asando <alexando17@yahoo.com>
2026-02-13 12:20:26 +0100

    ADD: expansion part folder and ft_expansion.c raw concept

---
+++ src/expansion/expansion.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   expansion.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: asando <asando@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/13 12:10:52 by asando            #+#    #+#             */
+/*   Updated: 2026/02/13 12:19:18 by asando           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "expansion.h"
+
+void	ft_expand_tokens(t_token *tokens, char **envp, int exit_status)
+{
+	while (tokens)
+	{
+		if (tokens->type == TOKEN_WORD && tokens->flag_quote == Q_DOUBLE)
+			tokens->value = ft_expand_value(tokens->value, envp, exit_status);
+		tokens = tokens->next_token;
+	}
+	return ;
+}
