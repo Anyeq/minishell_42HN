5ce506ed0fd2922584fe876859a2beccced0d280
asando <alexando17@yahoo.com>
2026-02-22 11:10:55 +0100

    ADD: expansion_case.c and put case handle function into the file

---
+++ src/expansion/expansion_case.c
@@ -0,0 +1,71 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   expansion_case.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: asando <asando@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/22 11:09:17 by asando            #+#    #+#             */
+/*   Updated: 2026/02/22 11:10:23 by asando           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "expansion.h"
+
+char	*ft_get_env_value(char **envp, char *key)
+{
+	int	len;
+	int	i;
+
+	i = 0;
+	if (key == NULL)
+		return (NULL);
+	len = ft_strlen(key);
+	while (envp[i])
+	{
+		if (ft_strncmp(envp[i], key, len) == 0 && envp[i][len] == '=')
+			return (envp[i] + len + 1);
+		i++;
+	}
+	return (NULL);
+}
+
+char	*ft_var_case(char **result, char *str, int i, char **envp)
+{
+	char	*key;
+	char	*val;
+	char	*return_str;
+	int		start;
+
+	start = i;
+	return_str = NULL;
+	while (ft_isalnum(str[i]) || str[i] == '_')
+		i++;
+	key = ft_substr(str, start, i - start);
+	val = ft_get_env_value(envp, key);
+	if (val)
+		return_str = ft_join_and_free(*result, ft_strdup(val));
+	else if (val == NULL)
+		return_str = ft_join_and_free(*result, ft_strdup(""));
+	*result = NULL;
+	free(key);
+	return (return_str);
+}
+
+char	*ft_exit_status_case(char **result, char *str, int i, char **envp)
+{
+	char	*return_val;
+	char	*str_exit_status;
+
+	str_exit_status = ft_itoa(exit_status);
+	if (str_exit_status == NULL)
+	{
+		perror("malloc error");
+		return (NULL);
+	}
+	return_val = ft_join_and_free(*result, str_exit_status);
+	if (return_val == NULL)
+		return (NULL);
+	*result = NULL;
+	return (return_val);
+}
