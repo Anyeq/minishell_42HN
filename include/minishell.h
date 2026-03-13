fef34bc2ca5276e9fd1e830c824e5520896a1b8b
asando <alexando17@yahoo.com>
2026-03-13 16:53:47 +0100

    FIX: Modifying header for minishell and signalmnshell.h

--- include/minishell.h
+++ include/minishell.h
@@ -6,7 +6,7 @@
 /*   By: asando <asando@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/16 13:54:25 by asando            #+#    #+#             */
-/*   Updated: 2026/03/13 16:25:12 by asando           ###   ########.fr       */
+/*   Updated: 2026/03/13 16:53:19 by asando           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,9 @@
 # include "parse.h"
 # include "exepansion.h"
 # include "execution.h"
+# include "signalmnshell,h"
+# include <readline/readline.h>
+# include <readline/history.h>
 # include "env.h"
 
 //UTILS
--- include/signalmnshell.h
+++ include/signalmnshell.h
@@ -6,7 +6,7 @@
 /*   By: asando <asando@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/25 15:27:16 by asando            #+#    #+#             */
-/*   Updated: 2026/02/25 15:30:41 by asando           ###   ########.fr       */
+/*   Updated: 2026/03/13 16:53:23 by asando           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,7 @@
 
 #include <signal.h>
 #include <unistd.h>
-#include <readline/readline.h>
 
-void	ft_sigint_handler_shell(int sig);
-void	ft_sigint_handler_heredoc(int sig);
 void	ft_setup_signals_shell(void);
 void	ft_setup_signals_heredoc(void);
 void	ft_setup_signals_child(void);
